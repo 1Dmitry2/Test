e5f181f20dba9e14a2fb161dc0ec05172ea28ca7
Dmitry <pain2284@gmail.com>
2023-05-18 15:56:26 +0500

    ok
    Тестовое задание готово

---
+++ vue-test/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import Metrics from "./components/Metrics.vue";
+import Button from './components/Button.vue'
+export default {
+    components:{
+        Metrics,
+        Button,
+        Modal
+    }
+}
+</script>
+
+<template>
+  <div>
+      <Button/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
