165923aaa3539ace7fc6e4b5f1dcdce930ee4849
Dmitry <pain2284@gmail.com>
2023-05-22 13:21:17 +0500

    Готово
    Готово

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import Accordion from './components/Accordion.vue'
+</script>
+
+<template>
+  <Accordion 
+  title="Водоизмещение"
+  :items="[
+    {name: 'Судно порожнем', value1: '8 296.50', unit: 'T'},
+    {name: 'Дедвейт', value1: '20 935.5', value2: '25 000', unit: 'T'},
+    {name: 'Тру', value: true},
+    {name: 'Фолз', value: false},
+    {name: 'Водоизмещение', value1: '29 242.04', value2: '25 000', unit: 'T'},
+    {name: 'Водоизмещение по ЛГМ', value1: '33 417', value2: '25 000', unit: 'T'},
+    {name: 'Груз', value1: '19 401.6', value2: '43 554.7', unit: 'T'},
+    {name: 'Жидкий балласт', value1: '0', value2: '11 147.59', unit: 'T'},
+    {name: 'Тяжелое топливо', value1: '982.5', value2: '892.44', unit: 'T'},
+    {name: 'Дизельное топливо', value1: '113.3', value2: '113.30', unit: 'T'},
+    {name: 'Пресная вода', value1: '152.2', value2: '152.10', unit: 'T'},
+    {name: 'Смазочное масло', value1: '37.89', value2: '42.10', unit: 'T'},
+    {name: 'Постоянные нагрузки', value1: '45', unit: 'T'},
+    ]" />
+      <Accordion 
+  title="Посадка судна"
+  :items="[
+    {name: 'Дифферент', value1: '-1.75', unit: 'м'},
+    {name: 'Угол крена', value1: '0', unit: '°'},
+    {name: 'Заглубление винта', value1: '99', unit: '%'},
+    {name: 'Осадка носом', value1: '9.68', unit: 'м'},
+    {name: 'Осадка кормой', value1: '8.8', unit: 'м'},
+    {name: 'Осадка на миделе', value1: '9.67', unit: 'м'},
+    {name: 'Средняя осадка', value1: '10.55', unit: 'м'},
+    ]" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Accordion.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+let truePlaceholder = "Да"
+let falsePlaceholder = "Нет"
+</script>
+
+<script lang="ts">
+
+export default {
+  props: ['title', 'items'],
+
+  data() {
+    return {
+      accordionOpen: false
+    }
+  },
+
+  methods: {
+    toggleOpen() {
+      this.accordionOpen = !this.accordionOpen
+      console.log(this.accordionOpen)
+    }
+}
+}
+</script>
+
+<template>
+  <div class="">
+    <div class="flex justify-between cursor-pointer w-96" @click="toggleOpen">
+      <h3 class="text-left font-semibold pb-2">{{ title }}</h3>
+      <img src="/arrow.svg" class="" alt="">
+    </div>
+
+    <div v-for="item in items" class="transition-all" :class="accordionOpen ? 'opacity-0' : 'opacity-100'">
+      <div class="grid grid-cols-2 gap-5 text-left pb-2" :class="{ hidden : accordionOpen}">
+        <div>{{ item.name }}</div>
+        <div>{{ typeof(item.value) == 'boolean' ? 
+        item.value ? truePlaceholder : falsePlaceholder : (item.value2 ? 
+        item.value1 + ' / ' + item.value2 + " " + item.unit : item.value1 + " " + item.unit) }}</div>
+      </div>
+      <hr />
+    </div>
+    <hr/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
